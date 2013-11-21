01f4d161fc358c919ce4c78d15677bdcef2923f8
Ivan Silin <naawha@gmail.com>
2013-11-22 01:53:42 +0400

    Сделал автокомплиты в поле с населённым пунктом

--- kladr/widgets.py
+++ kladr/widgets.py
@@ -25,7 +25,6 @@ class KladrSelectWidget(Select):
 
 
 class KladrTextWidget(Select):
-
     def __init__(self, parent_name,  action, accept_empty, empty_label, grandparent_name='', attrs=None, choices=()):
         self.datas = {
             'data-parent-id': 'id_%s' % parent_name,
@@ -38,15 +37,39 @@ class KladrTextWidget(Select):
         self.choices = list(choices)
         super(Select, self).__init__(attrs)
 
+    # def render(self, name, value, attrs=None, choices=()):
+    #     display_attrs = dict(self.datas, **{'class': 'kladr', 'id': 'id_%s_display' % name })
+    #     if value is None:
+    #         value = ''
+    #     final_attrs = self.build_attrs(attrs, name=name)
+    #     if value != '':
+    #         final_attrs['value'] = value
+
+
     def render(self, name, value, attrs=None, choices=()):
-        display_attrs = dict(self.datas, **{'class': 'kladr', 'id': 'id_%s_display' % name })
+        displayed_id = 'id_%s_display' % name
+        hidden_id = 'id_%s' % name
+        values_for_autocomplete = '%s_data' % name
+        attrs = dict(self.datas, **{
+            'class': 'kladr',
+            'id': displayed_id,
+            'data-hidden-id': hidden_id,
+            'data-values': values_for_autocomplete
+        })
         if value is None:
             value = ''
-        final_attrs = self.build_attrs(attrs, name=name)
-        if value != '':
-            final_attrs['value'] = value
+        final_attrs = self.build_attrs(name=name, id=hidden_id, value=value)
+
+        display_value = ''
+        for k, v in self.choices:
+            if k == value:
+                display_value = v
+
         return render_to_string('kladr/widgets/kladr_text_widget.html', {
-            'display_attrs': flatatt(display_attrs),
+            'display_attrs': flatatt(attrs),
             'final_attrs': flatatt(final_attrs),
-            'choices': choices
+            'choices': self.choices,
+            'values_var': values_for_autocomplete,
+            'display_value': display_value
         })
+
--- staticfiles/js/delayed_fill.js
+++ staticfiles/js/delayed_fill.js
@@ -52,3 +52,97 @@
         });
     });
 })(jQuery || django.jQuery);
+
+
+
+
+
+
+(function($) {
+    $(document).ready(function(){
+        function fill_empty(target, label, autocomplete_values) {
+            window[autocomplete_values] = [{
+                'value': "",
+                'label': label
+            }];
+            target.autocomplete('option', 'source', window[autocomplete_values]);
+        }
+
+        function clean_fields(target, hidden) {
+            target.val('');
+            hidden.val('');
+        }
+
+
+
+        function fill_field(target, label, url, pk, parent_pk, autocomplete_values) {
+            var value = (pk)? pk : 0;
+
+            $.getJSON(url + value + '/' + parent_pk + '/', function(j) {
+                console.log(j.length);
+                window[autocomplete_values] = [{
+                    'value': "",
+                    'label': label
+                }];
+                for (var i = 0; i < j.length; i++) {
+                    window[autocomplete_values].push({
+                        'value': j[i].value,
+                        'label': j[i].display
+                    });
+                }
+                target.autocomplete('option', 'source', window[autocomplete_values]);
+            });
+        }
+
+
+
+        $('input[type=text].kladr').each(function() {
+            var $parent = $('#' + $(this).data('parent-id')),
+                $hidden = $('#' + $(this).data('hidden-id')),
+                $target = $(this),
+                autocomplete_values = $(this).data('values'),
+                url = $(this).data('url'),
+                empty_label = $(this).data('empty-label'),
+                accept_empty = $(this).data('accept-empty') == 'True';
+
+            $target.autocomplete({
+                minLength: 0,
+                source: window[autocomplete_values],
+                focus: function( event, ui ) {
+                    $target.val( ui.item.label );
+                    return false;
+                },
+                select: function( event, ui ) {
+                    $target.val( ui.item.label );
+                    $hidden.val( ui.item.value );
+                    return false;
+                }
+            })
+                .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+                return $( "<li>" )
+                    .append( "<a>" + item.label + "</a>" )
+                    .appendTo( ul );
+            };
+
+            $parent.on('change', function() {
+                var parent_disabled = this.disabled == true;
+                var pk = $(this).val();
+                var parent_pk = 0;
+                if (pk == 0 && accept_empty && !parent_disabled){
+                    var $grandparent = $('#' + $(this).data('parent-id'));
+                    parent_pk = $grandparent.val();
+                }
+
+                if (!accept_empty && (!pk || pk == '') || parent_disabled) {
+                    fill_empty($target, empty_label, autocomplete_values);
+                    $target.attr('disabled', true);
+                } else {
+                    fill_field($target, empty_label, url, pk, parent_pk, autocomplete_values);
+                    $target.attr('disabled', false);
+                }
+                $target.trigger('change');
+                clean_fields($target, $hidden);
+            });
+        });
+    });
+})(jQuery || django.jQuery);
